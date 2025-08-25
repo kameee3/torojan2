93d3943893148fc8006018053caf027f6df4a306
kameee3 <u0803096@gmail.com>
2025-08-26 07:57:25 +0900

    Update torojanb.bat

--- torojan-main/torojanb.bat
+++ torojan-main/torojanb.bat
@@ -8,6 +8,8 @@ set /a CURRENT=0
 
 :loop
 set /a CURRENT+=1
+powershell -c "[System.Media.SystemSounds]::Exclamation.Play()"
+
 start "" mshta "javascript:var r=confirm('%MSG%'); if(r){alert('Your PC has been infected with a virus!');} else {alert('Your PC has been infected with a virus!');}; close();"
 echo ^G
 
@@ -24,4 +26,5 @@ if exist "torojank.bat" (
 
 pause
 endlocal
+
 
