2ca0f3c31bb40308c02c8193e57de97ac5b7e40d
kameee3 <u0803096@gmail.com>
2025-08-26 07:19:46 +0900

    Update torojanb.bat

--- torojan-main/torojanb.bat
+++ torojan-main/torojanb.bat
@@ -1,21 +1,4 @@
 @echo off
-setlocal
-
-
-set "MSG=Warning! A system error has occurred!"
-set "COUNT=100"  
-
-
-echo ^G
-
-
 :loop
-set /a COUNT-=1
-if %COUNT% leq 0 goto end
-mshta "javascript:alert('%MSG%');close();"
+msg * "Your PC has been infected with a virus!"
 goto loop
-
-:end
-echo 
-pause
-endlocal
